389d85d2fd80240a3bd2fd2e35c6c96decb0009b
simona <simona.fabbri194@gmail.com>
2024-10-14 19:50:58 +0200

    animazione pagina principale

--- src/app/Contacts/[Contact]/page.js
+++ src/app/Contacts/[Contact]/page.js
@@ -2,15 +2,18 @@
 
 import { useState } from "react";
 import Link from "next/link";
+import Footer from "@/components/Footer";
 
 export default function ContactPage({ params }) {
-  const [status, setStatus] = useState("");
-  const [nameValue, setNameValue] = useState("Name");
-  const [emailValue, setEmailValue] = useState("Email");
-  const [messageValue, setMessageValue] = useState("Message");
+  const [formState, setFormState] = useState("input"); // "input", "loading", "success"
+  const [nameValue, setNameValue] = useState("");
+  const [emailValue, setEmailValue] = useState("");
+  const [messageValue, setMessageValue] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setFormState("loading");
+
     const form = e.target;
     const data = new FormData(form);
 
@@ -26,119 +29,125 @@ export default function ContactPage({ params }) {
       const result = await response.json();
 
       if (result.success) {
-        setStatus("Grazie! Il tuo messaggio è stato inviato.");
-        form.reset();
+        setFormState("success");
+        setNameValue("");
+        setEmailValue("");
+        setMessageValue("");
       } else {
-        setStatus("Oops! C'è stato un problema nell'invio del messaggio.");
+        setFormState("input");
+        alert("Oops! There was a problem sending the message.");
       }
     } catch (error) {
-      setStatus("Oops! C'è stato un problema nell'invio del messaggio.");
+      setFormState("input");
+      alert("Oops! There was a problem sending the message.");
     }
   };
 
-  const handleNameFocus = (e) => {
-    if (e.target.value === "Name") {
-      setNameValue("");
-    }
-  };
-
-  const handleNameBlur = (e) => {
-    if (e.target.value === "") {
-      setNameValue("Name");
-    }
-  };
-
-  const handleEmailFocus = (e) => {
-    if (e.target.value === "Email") {
-      setEmailValue("");
-    }
-  };
-
-  const handleEmailBlur = (e) => {
-    if (e.target.value === "") {
-      setEmailValue("Email");
-    }
-  };
-
-  const handleMessageFocus = () => {
-    if (messageValue === "Message") {
-      setMessageValue("");
-    }
-  };
-
-  const handleMessageBlur = () => {
-    if (messageValue.trim() === "") {
-      setMessageValue("Message");
+  const renderFormContent = () => {
+    switch (formState) {
+      case "loading":
+        return (
+          <div className="flex justify-center items-center h-64">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-[#DE2A4A]"></div>
+          </div>
+        );
+      case "success":
+        return (
+          <div className=" p-8  flex flex-col items-center">
+            <svg
+              className="w-16 h-16 text-black mb-4 bg-[#DE2A4A] rounded-full"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 13l4 4L19 7"
+              ></path>
+            </svg>
+            {/* <p className="text-xl font-semibold text-white">
+              Message Sent Successfully!
+            </p> */}
+          </div>
+        );
+      default:
+        return (
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div className="md:flex w-full md:space-x-4 md:space-y-0 space-y-2">
+              <div className="md:w-[50%]">
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={nameValue}
+                  onChange={(e) => setNameValue(e.target.value)}
+                  required
+                  placeholder="Name"
+                  className="w-full px-3 py-2 border rounded-md placeholder-[#808080]  text-black text-[16px]"
+                />
+              </div>
+              <div className="md:w-[50%]">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={emailValue}
+                  onChange={(e) => setEmailValue(e.target.value)}
+                  required
+                  placeholder="Email"
+                  className="w-full px-3 py-2 border rounded-md placeholder-[#808080]  text-black  text-[16px]"
+                />
+              </div>
+            </div>
+            <div>
+              <textarea
+                id="message"
+                name="message"
+                value={messageValue}
+                onChange={(e) => setMessageValue(e.target.value)}
+                required
+                placeholder="Message"
+                className="w-full px-3 py-2 border rounded-md placeholder-[#808080]  text-black  text-[16px]"
+                rows="4"
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="w-full text-[20px] bg-[#DE2A4A] text-black font-bold py-4 px-4 rounded-md hover:bg-opacity-80"
+            >
+              SEND
+            </button>
+          </form>
+        );
     }
   };
 
   return (
-    <div className=" max-w-[1200px] mx-auto px-4 ">
-      <div className="w-full max-w-[960px] mx-auto">
-        <div className="pt-[80px]">
-          <Link
-            href="/"
-            className="text-[#DE2A4A] text-[22px] font-mono hover:opacity-[0.6]"
-          >
-            <span>&lt;</span> BACK
-          </Link>
+    <div className="relative">
+      <div className="max-w-[1200px] mx-auto px-4">
+        <div className="w-full max-w-[960px] mx-auto">
+          <div className="pt-[80px]">
+            <Link
+              href="/"
+              className="text-[#DE2A4A] text-[22px] font-mono hover:opacity-[0.6]"
+            >
+              <span>&lt;</span> BACK
+            </Link>
+          </div>
+
+          <h1 className="font-mono md:text-[150px] text-[90px] lg:text-[220px] text-[white]">
+            CONTACT
+          </h1>
+
+          <div className="md:space-y-4 space-y-2 max-w-[60%] md:pb-[200px] pb-[90px]">
+            {renderFormContent()}
+          </div>
         </div>
-
-        <h1 className="font-mono  md:text-[150px] text-[90px] lg:text-[220px] text-[white] ">
-          CONTACT
-        </h1>
-
-        <form onSubmit={handleSubmit} className="md:space-y-4 space-y-2">
-          <div className="md:flex w-full md:space-x-4 md:space-y-0 space-y-2">
-            <div className="md:w-[50%]">
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={nameValue}
-                onChange={(e) => setNameValue(e.target.value)}
-                onFocus={handleNameFocus}
-                onBlur={handleNameBlur}
-                required
-                className="w-full px-3 py-2 border rounded-md text-[#808080] text-[16px]"
-              />
-            </div>
-            <div className="md:w-[50%]">
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={emailValue}
-                onChange={(e) => setEmailValue(e.target.value)}
-                onFocus={handleEmailFocus}
-                onBlur={handleEmailBlur}
-                required
-                className="w-full px-3 py-2 border rounded-md text-[#808080] text-[16px]"
-              />
-            </div>
-          </div>
-          <div>
-            <textarea
-              id="message"
-              name="message"
-              value={messageValue}
-              onChange={(e) => setMessageValue(e.target.value)}
-              onFocus={handleMessageFocus}
-              onBlur={handleMessageBlur}
-              required
-              className="w-full px-3 py-2 border rounded-md text-[#808080] text-[16px]"
-              rows="4"
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="w-full text-[20px] bg-[#DE2A4A] text-black font-bold  py-4 px-4 rounded-md hover:bg-opacity-80 "
-          >
-            SEND
-          </button>
-        </form>
       </div>
-      {status && <p className="mt-4 text-center">{status}</p>}
+      <Footer />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,17 @@
 "use client";
 
-import { useState, useEffect, useCallback } from "react";
-import SelectWork from "../components/SelectWork.js";
-import WorkTogether from "../components/WorkTogether.js";
-import Footer from "./Footer.js";
+import { useState, useEffect, useCallback, useRef, useMemo } from "react";
+import { motion, useInView } from "framer-motion";
+import SelectWork from "./SelectWork.js";
+import WorkTogether from "./WorkTogether.js";
 
 const Home = () => {
   const [isHovered, setIsHovered] = useState(false);
+  const simonaTextRef = useRef(null);
 
   const handleMouseMove = useCallback((e) => {
-    const element = document.getElementById("simona-text");
-    if (element) {
-      const rect = element.getBoundingClientRect();
+    if (simonaTextRef.current) {
+      const rect = simonaTextRef.current.getBoundingClientRect();
       const isInside =
         e.clientX >= rect.left &&
         e.clientX <= rect.right &&
@@ -26,26 +26,62 @@ const Home = () => {
     return () => window.removeEventListener("mousemove", handleMouseMove);
   }, [handleMouseMove]);
 
+  const fadeInUp = useMemo(
+    () => ({
+      hidden: { opacity: 0, y: 20 },
+      visible: { opacity: 1, y: 0 },
+    }),
+    []
+  );
+
+  const ScrollAnimatedSection = useCallback(
+    ({ children }) => {
+      const ref = useRef(null);
+      const isInView = useInView(ref, { once: true, amount: 0.3 });
+
+      return (
+        <motion.div
+          ref={ref}
+          initial="hidden"
+          animate={isInView ? "visible" : "hidden"}
+          variants={fadeInUp}
+          transition={{ duration: 0.5 }}
+        >
+          {children}
+        </motion.div>
+      );
+    },
+    [fadeInUp]
+  );
+
   return (
     <div className="w-full max-w-[1200px] mx-auto">
       <div className="w-full px-[20px] md:px-[60px] lg:px-[120px] pb-[60px] md:pb-[100px] lg:pb-[120px] pt-[80px]">
         <div>
-          <h1 className="text-[90px] md:text-[150px] lg:text-[220px] text-white font-mono  leading-none ">
+          <motion.h1
+            className="text-[90px] md:text-[150px] lg:text-[220px] text-white font-mono leading-none"
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+          >
             HI, I'M {""}
-            <span className="relative ">
+            <span className="relative">
               <span
-                id="simona-text"
+                ref={simonaTextRef}
                 className="text-[#DE2A4A] font-mono transition-opacity duration-300"
                 style={{
                   opacity: isHovered ? 0 : 1,
                 }}
               >
                 SIMONA
-                <span className="text-white ">.</span>
+                <span className="text-white">.</span>
               </span>
-              <div
+              <motion.div
                 className="absolute top-0 left-0 w-full h-full flex items-center justify-start transition-opacity duration-300"
                 style={{ opacity: isHovered ? 1 : 0 }}
+                initial={{ scale: 0 }}
+                animate={{ scale: isHovered ? 1 : 0 }}
+                transition={{ duration: 0.3 }}
               >
                 <div className="lg:w-[180px] lg:h-[180px] md:w-[140px] md:h-[140px] w-[80px] h-[80px] rounded-full overflow-hidden">
                   <img
@@ -54,70 +90,81 @@ const Home = () => {
                     className="w-full h-full object-cover object-[37.6%_23.2%]"
                   />
                 </div>
-              </div>
+              </motion.div>
             </span>
-          </h1>
+          </motion.h1>
           <div className="pt-[40px] lg:pt-[100px] md:pt-[70px] lg:pb-[100px] md:pb-[70px] pb-[40px] max-w-[1000px] flex justify-center items-center  ">
             <div className="lg:px-[70px] md:px-[35px]">
               <div className="md:flex-col flex-row">
-                <div className="flex flex-col md:flex-row ">
-                  <div className="mr-[60px] md:w-[20%] ">
-                    <h3 className="lg:text-[22px] md:text-[18px] text-[14px] text-white font-bold pb-[15px] md:pb-[0px]">
-                      <span className="lg:hidden ">WEB DEVELOPER</span>
-                      <span className="hidden lg:inline">
-                        WEB
-                        <br /> DEVELOPER
-                      </span>
-                    </h3>
+                <ScrollAnimatedSection>
+                  <div className="flex flex-col md:flex-row ">
+                    <div className="mr-[60px] md:w-[20%] ">
+                      <h3 className="lg:text-[22px] md:text-[18px] text-[14px] text-white font-bold pb-[15px] md:pb-[0px]">
+                        <span className="lg:hidden ">WEB DEVELOPER</span>
+                        <span className="hidden lg:inline">
+                          WEB
+                          <br /> DEVELOPER
+                        </span>
+                      </h3>
+                    </div>
+                    <div className="md:w-[70%] ">
+                      <p className="lg:text-[16px] md:text-[14px] text-[12px] font-normal text-[#AAAAAA] lg:pb-[80px] pb-[40px]">
+                        Hello! I'm Simona Fabbri, a passionate web developer
+                        with a fervent passion for creating engaging and
+                        innovative digital experiences.
+                        <br />I have experience in front-end development with
+                        skills in various technologies and programming
+                        languages.
+                        <br />I am passionate, creative, and determined to
+                        achieve high-quality results.
+                        <br />I strive to understand my clients' needs and
+                        deliver tailored web solutions that meet their
+                        expectations.
+                        <br />I am always seeking new challenges and
+                        opportunities to improve my skills and grow
+                        professionally.
+                      </p>
+                    </div>
                   </div>
-                  <div className="md:w-[70%] ">
-                    <p className="lg:text-[16px] md:text-[14px] text-[12px] font-normal text-[#AAAAAA] lg:pb-[80px] pb-[40px]">
-                      Hello! I'm Simona Fabbri, a passionate web developer with
-                      a fervent passion for creating engaging and innovative
-                      digital experiences.
-                      <br />I have experience in front-end development with
-                      skills in various technologies and programming languages.
-                      <br />I am passionate, creative, and determined to achieve
-                      high-quality results.
-                      <br />I strive to understand my clients' needs and deliver
-                      tailored web solutions that meet their expectations.
-                      <br />I am always seeking new challenges and opportunities
-                      to improve my skills and grow professionally.
-                    </p>
+                </ScrollAnimatedSection>
+                <ScrollAnimatedSection>
+                  <div className="flex flex-col md:flex-row ">
+                    <div className="mr-[60px] md:w-[20%]  ">
+                      <h3 className="lg:text-[22px] md:text-[18px] text-[14px] text-white font-bold pb-[15px] md:pb-[0px]">
+                        SKILLS
+                      </h3>
+                    </div>
+                    <div className="md:w-[70%]">
+                      <p className="lg:text-[16px] md:text-[14px] text-[12px] font-normal text-[#AAAAAA] lg:pb-[80px] pb-[40px] ">
+                        I am passionate about front-end development and have
+                        experience using HTML, CSS, and JavaScript to create
+                        intuitive and responsive user interfaces.
+                        <br />I have worked with various frameworks and
+                        libraries, including React.js, Next.js, and Tailwind
+                        CSS, to create dynamic and modern web experiences.
+                        <br />I also have skills in low-code and no-code
+                        development using tools like Framer and Webflow.
+                        <br />
+                        These platforms enable the creation of interactive
+                        prototypes and websites while minimizing the need for
+                        custom code, thereby accelerating the development
+                        process and offering greater flexibility in design.
+                        <br />I am meticulous and committed to adhering to the
+                        design provided to me in every project I undertake.
+                        <br /> I strive to pay close attention to details,
+                        ensuring that the design specifications are faithfully
+                        implemented.
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <div className="flex flex-col md:flex-row ">
-                  <div className="mr-[60px] md:w-[20%]  ">
-                    <h3 className="lg:text-[22px] md:text-[18px] text-[14px] text-white font-bold pb-[15px] md:pb-[0px]">
-                      SKILLS
-                    </h3>
-                  </div>
-                  <div className="md:w-[70%]">
-                    <p className="lg:text-[16px] md:text-[14px] text-[12px] font-normal text-[#AAAAAA] lg:pb-[80px] pb-[40px] ">
-                      I am passionate about front-end development and have
-                      experience using HTML, CSS, and JavaScript to create
-                      intuitive and responsive user interfaces.
-                      <br />I have worked with various frameworks and libraries,
-                      including React.js, Next.js, and Tailwind CSS, to create
-                      dynamic and modern web experiences.
-                      <br />I also have skills in low-code and no-code
-                      development using tools like Framer and Webflow.
-                      <br />
-                      These platforms enable the creation of interactive
-                      prototypes and websites while minimizing the need for
-                      custom code, thereby accelerating the development process
-                      and offering greater flexibility in design.
-                      <br />I am meticulous and committed to adhering to the
-                      design provided to me in every project I undertake.
-                      <br /> I strive to pay close attention to details,
-                      ensuring that the design specifications are faithfully
-                      implemented.
-                    </p>
-                  </div>
-                </div>
-                <SelectWork />
+                </ScrollAnimatedSection>
+                <ScrollAnimatedSection>
+                  <SelectWork />
+                </ScrollAnimatedSection>
                 <div className="pb-[40px] md:pb-[90px] lg:pb-[140px]"></div>
-                <WorkTogether />
+                <ScrollAnimatedSection>
+                  <WorkTogether />
+                </ScrollAnimatedSection>
               </div>
             </div>
           </div>
